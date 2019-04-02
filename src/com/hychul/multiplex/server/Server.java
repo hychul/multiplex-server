92653c9061662e8464a79a297da043c6560a0389
hychul <hychome@gmail.com>
2019-04-02 15:01:34 +0900

    Creating dispatcher runnable

--- src/com/hychul/multiplex/server/Server.java
+++ src/com/hychul/multiplex/server/Server.java
@@ -9,9 +9,11 @@ import java.nio.channels.ServerSocketChannel;
 import com.hychul.multiplex.server.handler.AcceptHandler;
 import com.hychul.multiplex.server.handler.Handler;
 
-public class Server implements Runnable {
+public class Server {
     private final Selector selector;
     private final ServerSocketChannel serverSocketChannel;
+
+    private Thread dispatcherThread;
 
     public Server(int port) throws IOException {
         selector = Selector.open();
@@ -19,37 +21,40 @@ public class Server implements Runnable {
         serverSocketChannel.socket().bind(new InetSocketAddress(port));
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT).attach(new AcceptHandler(selector, serverSocketChannel));
+        dispatcherThread = new Thread(null, new Dispatcher(), "dispatcher-thread");
     }
 
     public void start() {
-        new Thread(this).start();
+        dispatcherThread.start();
     }
 
-    @Override
-    public void run() {
-        System.out.println("listening port: \"" + serverSocketChannel.socket().getLocalPort() + "\"");
-        try {
-            while (!Thread.interrupted()) {
-                selector.select();
-                selector.selectedKeys().forEach(this::dispatch);
-                selector.selectedKeys().clear();
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private void dispatch(SelectionKey key) {
-        var handler = (Handler) key.attachment();
-
-        try {
-            handler.handle();
-        } catch (IOException ex) {
-            ex.printStackTrace();
+    class Dispatcher implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("listening port: \"" + serverSocketChannel.socket().getLocalPort() + "\"");
             try {
-                key.channel().close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                while (!Thread.interrupted()) {
+                    selector.select();
+                    selector.selectedKeys().forEach(this::dispatch);
+                    selector.selectedKeys().clear();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        private void dispatch(SelectionKey key) {
+            var handler = (Handler) key.attachment();
+
+            try {
+                handler.handle();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                try {
+                    key.channel().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
